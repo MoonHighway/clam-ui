17ace1e1c9b835158c630aa59c7e76f333f67de8
Alex Banks <alex@moonhighway.com>
2018-10-25 10:09:56 -0700

    Adds Difficulty Icons (#53)
    * Adds Difficulty Icons
    * adds alt to difficulty image

--- src/components/screens/Welcome.js
+++ src/components/screens/Welcome.js
@@ -1,26 +1,34 @@
 import React from 'react'
 import { Link } from '@reach/router'
 import styled from 'styled-components'
+import { Difficulty } from '../ui'
 
-export const Welcome = () =>
-    <Container>
-        <h1>Welcome Screen</h1>
-        <Link to="/activities">Activities</Link>
-        <Link to="/counselors">Counselors</Link>
-        <Link to="/account">Login</Link>
-        <Link to="/account">Account</Link>
-        <Link to="/cabin/sheep">Cabin</Link>
-        <Link to="/schedule">Schedule</Link>
-    </Container>
+export const Welcome = () => (
+  <Container>
+    <h1>Welcome Screen</h1>
+    <Link to="/activities">Activities</Link>
+    <Link to="/counselors">Counselors</Link>
+    <Link to="/account">Login</Link>
+    <Link to="/account">Account</Link>
+    <Link to="/cabin/sheep">Cabin</Link>
+    <Link to="/schedule">Schedule</Link>
+    <div>
+      <h2>Difficulty Icons</h2>
+      <Difficulty />
+      <Difficulty level="INTERMEDIATE" />
+      <Difficulty level="ADVANCED" />
+      <Difficulty level="EXPERT" />
+    </div>
+  </Container>
+)
 
 const Container = styled.section`
+  /* temporary styles */
 
-    /* temporary styles */
+  display: flex;
+  flex-direction: column;
 
-    display: flex;
-    flex-direction: column;
-
-    * { 
-        margin: 1em 0;
-    }
+  * {
+    margin: 1em 0;
+  }
 `
