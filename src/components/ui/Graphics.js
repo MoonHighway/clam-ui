ca357b1ec72fcc5a811d9528d6efb32c17c512b2
MoonTahoe <alex@moonhighway.com>
2018-10-24 18:50:32 -0700

    Adds Difficulty Icons

--- src/components/ui/Graphics.js
+++ src/components/ui/Graphics.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import greenCircle from '../../assets/green-circle.svg'
+import blueSquare from '../../assets/blue-square.svg'
+import blackDiamond from '../../assets/single-black-diamond.svg'
+import doubleBlackDiamond from '../../assets/double-black-diamond.svg'
 
-export const Logo = () =>
-    <h3>Logo</h3>
+const difficulty = {
+  BEGINNER: greenCircle,
+  INTERMEDIATE: blueSquare,
+  ADVANCED: blackDiamond,
+  EXPERT: doubleBlackDiamond
+}
 
-export const Grass = () =>
-    <h3>Logo</h3>
+export const Logo = () => <h3>Logo</h3>
 
-export const Difficulty = () =>
-    <h3>Logo</h3>
-    
-export const Terrain = () =>
-    <h3>Terrain</h3>
+export const Grass = () => <h3>Logo</h3>
+
+export const Difficulty = ({ level = 'BEGINNER', height = 30, width = 30 }) => (
+  <img src={difficulty[level]} height={height} width={width} />
+)
+
+export const Terrain = () => <h3>Terrain</h3>
