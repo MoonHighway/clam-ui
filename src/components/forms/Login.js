cc46e943c16b9df4921099009d901f90d9a30d54
Alex Banks <alex@moonhighway.com>
2018-11-15 16:25:18 -0800

    50 apollo config (#56)
    * Removes env file from repo
    
    * Apollo Client Setup
    ===================
    
    * Configures Apollo Client
    * Configures Cache Persistance
    * Configures Apollo WebSocket Support
    * Adds Client Authorization Headers
    * Adds Websocket Authorization Headers
    * Stubs Query for github login link on login page
    
    * Adds package for subscriptions transport

--- src/components/forms/Login.js
+++ src/components/forms/Login.js
@@ -1,3 +1,19 @@
 import React from 'react'
+import { Query } from 'react-apollo'
+import { gql } from 'apollo-boost'
 
-export const Login = () => <h2>Login Form</h2>
+const GITHUB_AUTH_LINK = gql`
+  query {
+    githubAuthUrl
+  }
+`
+
+export const Login = () => (
+  <Query query={GITHUB_AUTH_LINK}>
+    {({ loading, data, error }) => {
+      if (loading) return <p>loading... </p>
+      if (error) return <p>{error}</p>
+      return <p>{JSON.stringify(data)}</p>
+    }}
+  </Query>
+)
