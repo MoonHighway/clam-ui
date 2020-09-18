3c441e9623b1ff2917c618ecb0f169a61ef11994
eveporcello <eve@moonhighway.com>
2020-09-17 17:26:59 -0700

    progress on form elements

--- src/components/forms/Login.js
+++ src/components/forms/Login.js
@@ -1,21 +1,68 @@
 import React from "react";
-import { useQuery, gql } from "@apollo/client";
+import styled from "styled-components";
+import { FaGithub } from "react-icons/fa";
 
-const GITHUB_AUTH_LINK = gql`
-  query {
-    githubAuthUrl
+export function Login() {
+  return (
+    <Container>
+      <form>
+        <div>
+          <label>Email</label>
+          <br />
+          <input type="email" />
+          <br />
+          <label>Password</label>
+          <br />
+          <input type="password" />
+        </div>
+        <button>Log In</button>
+        <button className="githubButton">
+          <FaGithub />
+          &nbsp;&nbsp;Log In with GitHub
+        </button>
+      </form>
+    </Container>
+  );
+}
+
+const Container = styled.section`
+  display: grid;
+  label,
+  input {
+    margin: 5px;
+  }
+  input {
+    font-size: 1.5em;
+    border: 0;
+    border-bottom: 1px solid #ccc;
+    border-radius: 0;
+    padding: 0;
+    cursor: text;
+    height: 1.75em;
+    width: 80%;
+  }
+  input:focus {
+    outline: 0;
+    border-bottom: 1px solid #666;
+  }
+
+  label {
+    text-transform: uppercase;
+    letter-spacing: 0.05em;
+  }
+
+  button {
+    background-color: #03a9f4;
+    border: none;
+    width: 100%;
+    text-align: center;
+    height: 45px;
+    border-radius: 4px;
+    color: white;
+    margin-top: 20px;
+    font-size: 1em;
+  }
+  .githubButton {
+    background-color: rgba(36, 41, 44, 0.9);
   }
 `;
-
-export const Login = () => {
-  const { data, loading, error } = useQuery(GITHUB_AUTH_LINK);
-  if (loading) return <p>loading... </p>;
-  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
-  return (
-    <>
-      <h2>Log In to Camp Lambda</h2>
-      <button>Log In</button>
-      <button>Log In with GitHub</button>
-    </>
-  );
-};
--- src/components/screens/Authorization.js
+++ src/components/screens/Authorization.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Login, Register } from "../forms";
 import { Header } from "../ui/Header";
 import styled from "styled-components";
 import logo from "../../assets/lambda.png";
 
 export function Authorization() {
+  const [login, setLogin] = useState(true);
   return (
     <Container>
       <Header />
-      <Login />
-      <Register />
-      <div className="logo">
+      <div className="form-holder">
+        <div>
+          <button
+            style={{
+              backgroundColor: `${login ? "gray" : "#D3D3D3"}`,
+              color: `${login ? "white" : "gray"}`
+            }}
+            className="tab"
+            onClick={() => setLogin(true)}
+          >
+            Log In
+          </button>
+          <button
+            style={{
+              backgroundColor: `${login ? "#D3D3D3" : "gray"}`,
+              color: `${login ? "gray" : "white"}`
+            }}
+            className="tab"
+            onClick={() => setLogin(false)}
+          >
+            Create Account
+          </button>
+          <div className="form">{login ? <Login /> : <Register />}</div>
+        </div>
+      </div>
+      <div className="tent">
         <h1>Camp Lambda</h1>
         <img src={logo} height={200} alt="camp lambda logo" />
       </div>
@@ -19,7 +43,32 @@ export function Authorization() {
 }
 
 const Container = styled.section`
+  display: grid;
+  grid-template-columns: 0.5fr 0.8fr 1.2fr 0.5fr;
+  grid-template-rows: 0.5fr 2fr 1fr 0.5fr;
+  .tab {
+    border: none;
+    width: 45%;
+    margin-bottom: 5%;
+    text-align: center;
+    height: 45px;
+    color: white;
+    margin-top: 20px;
+    font-size: 1em;
+  }
+  .form-holder {
+    grid-area: 2 / 2 / 3 / 3;
+    align-self: center;
+  }
   header {
     grid-area: 1 / 1 / 2 / 5;
+  }
+  .tent {
+    grid-area: 2 / 3 / 3 / 4;
+    justify-self: end;
+    h1 {
+      text-align: center;
+      font-size: 2em;
+    }
   }
 `;
